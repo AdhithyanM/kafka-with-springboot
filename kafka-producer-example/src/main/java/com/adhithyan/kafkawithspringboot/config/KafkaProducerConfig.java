ee7f46c53455ebbdec0f0369b57eaf93a7f9d77e
AdhithyanM <adhithyanmanoharan26@gmail.com>
2024-06-20 22:39:18 +0530

    configuring kafka using java-configuration rather with application.yml

--- kafka-producer-example/src/main/java/com/adhithyan/kafkawithspringboot/config/KafkaProducerConfig.java
+++ kafka-producer-example/src/main/java/com/adhithyan/kafkawithspringboot/config/KafkaProducerConfig.java
@@ -1,8 +1,17 @@
 package com.adhithyan.kafkawithspringboot.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Configuration class that sets up Kafka-related beans.
@@ -20,5 +29,24 @@ public class KafkaProducerConfig {
     public NewTopic customerTopic() {
         // Create a new topic named "customer-topic" with 3 partitions and a replication factor of 1.
         return new NewTopic("customer-topic", 3, (short) 1);
+    }
+
+    @Bean
+    public Map<String, Object> producerConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
