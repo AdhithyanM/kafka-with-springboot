8bcce411452cc0594f93880bf7faeb4221142cd3
AdhithyanM <adhithyanmanoharan26@gmail.com>
2024-06-20 20:06:23 +0530

    init kafka consumer example

---
+++ kafka-consumer-example/src/main/java/com/adhithyan/kafkawithspringboot/KafkaConsumerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.adhithyan.kafkawithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerExampleApplication.class, args);
+	}
+
+}
